45577daa7242a26e4ae2830c29c6a8114f22a55c
ukpono-j <ukponoakpan270@gmail.com>
2023-01-03 23:04:55 -0800

    first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
 
-	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
 
-return (0);
+		char c;
+
+
+
+			for (c = 'a'; c <= 'z'; c++)
+
+						putchar(c);
+
+					putchar('\n');
+
+
+
+					return (0);
+
 }
